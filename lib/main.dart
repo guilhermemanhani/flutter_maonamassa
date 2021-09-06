a81e951c42c3e3ad570da5e545be9615ddfff286
Guilherme Augusto da Rocha Manhani <gmanhani@sup05.unicred-parana.corp>
2021-09-06 10:10:38 -0300

    navegacao param

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:flutter_maonamassa/navegacao/page1.dart';
 import 'package:flutter_maonamassa/navegacao/page2.dart';
 import 'package:flutter_maonamassa/navegacao/page3.dart';
 import 'package:flutter_maonamassa/navegacao/page4.dart';
+import 'package:flutter_maonamassa/navegacao_params/detalhe.dart';
+import 'package:flutter_maonamassa/navegacao_params/lista.dart';
 // import 'package:flutter_maonamassa/home/home_page.dart';
 // import 'package:flutter_maonamassa/images/images_page.dart';
 
@@ -20,12 +22,15 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       // home: navegacao.HomePage(),
+      initialRoute: '/navegacao_param',
       routes: {
         '/': (_) => navegacao.HomePage(),
         '/page1': (_) => Page1(),
         Page2.routeName: (_) => Page2(),
         '/page3': (_) => Page3(),
         '/page4': (_) => Page4(),
+        '/navegacao_param': (_) => Lista(),
+        '/detalhe': (_) => Detalhe(),
       },
       // home: ImagesPage(),
     );
---
+++ lib/navegacao_params/detalhe.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class Detalhe extends StatefulWidget {
+  const Detalhe({Key? key}) : super(key: key);
+
+  @override
+  _DetalheState createState() => _DetalheState();
+}
+
+class _DetalheState extends State<Detalhe> {
+  int id = 0;
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance?.addPostFrameCallback((timeStamp) {
+      final param =
+          ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
+      setState(() {
+        id = param?['id'] ?? 0;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: Center(
+        child: Text('id é $id'),
+      ),
+    );
+  }
+}
